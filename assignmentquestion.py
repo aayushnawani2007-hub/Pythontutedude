ff57b37562f396310ccc0f37bc6c3d1df9aa8c4b
AAYUSH <aayushnawani2007@gmail.com>
2026-02-13 20:21:37 +0530

    added assignmentquestions

---
+++ assignmentquestion.py
@@ -0,0 +1,20 @@
+#1.  Takes two numbers as input from the user.
+#2.  Performs the basic mathematical operations on these two numbers:
+# o	Addition
+# o	Subtraction
+# o	Multiplication
+# o	Division
+# 3.  Displays the results of each operation on the screen.
+#  Expected Output:
+# The output should include the result of each operation performed, for example:
+
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("Addition",c)
+print("Subtraction",d)
+print("Multiplication",e)
+print("Division",f)
---
+++ secondquestion.py
@@ -0,0 +1,8 @@
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name.
+# Expected Output:
+# The program should output a greeting like:
+a=input("enter your first name:")
+b=input("enter your last name:")
+print("Hello,",a,b,"welcome to the python program")
